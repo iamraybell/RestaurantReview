742458b6ebecbed16d623328be8e3ca6293114c9
Ray Bell <raybell@macbook-pro-3.lan>
2018-10-01 19:38:08 -0700

    fixed small error in js file

--- js/restaurant_info.js
+++ js/restaurant_info.js
@@ -90,7 +90,7 @@ fillRestaurantHTML = (restaurant = self.restaurant) => {
   const image = document.getElementById('restaurant-img');
   image.className = 'restaurant-img'
   image.src = DBHelper.imageUrlForRestaurant(restaurant);
-  image.alt = `image of ${restaurant.name;}`
+  image.alt = `image of ${restaurant.name}`
   const cuisine = document.getElementById('restaurant-cuisine');
   cuisine.innerHTML = restaurant.cuisine_type;
 
