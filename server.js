c450d269db5c8695121d9c35987a56f3a260a394
Ray Bell <raybell@macbook-pro-3.lan>
2018-10-01 17:26:41 -0700

    changed to double quotes to match other files

--- server.js
+++ server.js
@@ -1,13 +1,13 @@
 // server file. This should be ran to access pages for this app.
-let path  = require('path'),
-	express = require('express'),
-	bp = require('body-parser');
+let path  = require("path"),
+	express = require("express"),
+	bp = require("body-parser");
 
 const port = 8000
 const app = express();
 
 // Currently we are just listeing in this directory. Could be changed if more files are added.
-app.use(express.static(path.join(__dirname, './')));
+app.use(express.static(path.join(__dirname, "./")));
 app.use(bp.urlencoded());
 
 
